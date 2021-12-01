49d028b09b7884b896f0212e5d03dc98a80415ce
Quentin Barrand <quentin@quba.fr>
2021-12-01 18:55:09 +0100

    Add day 1.

---
+++ day01/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"container/ring"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	fd, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fd.Close()
+
+	var (
+		inc  int
+		r    = ring.New(4)
+		v    int
+		wInc int
+	)
+
+	for i := 1; ; i++ {
+		_, err = fmt.Fscanf(fd, "%d", &v)
+
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			log.Fatalf("Failed while reading line %d: %v", i, err)
+		}
+
+		r.Value = v
+
+		if i > 1 && r.Prev().Value.(int) < v {
+			inc++
+		}
+
+		if i >= 4 {
+			pcw := r
+			cw := v
+
+			ppw := r.Prev()
+			pw := ppw.Value.(int)
+
+			for j := 0; j < 2; j++ {
+				pcw = pcw.Prev()
+				cw += pcw.Value.(int)
+
+				ppw = ppw.Prev()
+				pw += ppw.Value.(int)
+			}
+
+			if cw > pw {
+				wInc++
+			}
+		}
+
+		r = r.Next()
+	}
+
+	log.Printf("Part 1: %d", inc)
+	log.Printf("Part 2: %d", wInc)
+}
