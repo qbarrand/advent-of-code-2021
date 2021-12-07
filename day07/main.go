fee66c484e8c5bf27bc36f243d2983d81828b8e7
Quentin Barrand <quentin@quba.fr>
2021-12-08 00:28:19 +0100

    Add day 7.

---
+++ day07/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"math"
+	"strconv"
+
+	"github.com/qbarrand/advent-of-code-2021/util"
+)
+
+func getFuel(pos []int, midPoint int, moveCost func(int) int) int {
+	res := 0
+
+	for _, p := range pos {
+		var distance int
+
+		if p > midPoint {
+			distance = p - midPoint
+		} else {
+			distance = midPoint - p
+		}
+
+		res += moveCost(distance)
+	}
+
+	return res
+}
+
+func constantCost(n int) int {
+	return n
+}
+
+func sumCost(n int) int {
+	return (n * (n + 1)) / 2
+}
+
+func main() {
+	cl := util.ParseCommandLine()
+
+	fd := util.MustOpen(cl.InputFile)
+	defer fd.Close()
+
+	s := bufio.NewScanner(fd)
+	s.Split(util.ScanCommaSeparatedInts)
+
+	min := math.MaxInt32
+	max := 0
+
+	pos := make([]int, 0)
+
+	for s.Scan() {
+		if err := s.Err(); err != nil {
+			log.Fatalf("Error while scanning: %v", err)
+		}
+
+		t := s.Text()
+
+		n, err := strconv.ParseInt(t, 10, 32)
+		if err != nil {
+			log.Fatalf("Could not parse %q as an integer: %v", t, err)
+		}
+
+		i := int(n)
+
+		pos = append(pos, i)
+
+		if i > max {
+			max = i
+		}
+
+		if i < min {
+			min = i
+		}
+	}
+
+	part1 := math.MaxInt32
+	part2 := math.MaxInt32
+
+	for i := min; i <= max; i++ {
+		if fuel := getFuel(pos, i, constantCost); fuel < part1 {
+			part1 = fuel
+		}
+
+		if fuel := getFuel(pos, i, sumCost); fuel < part2 {
+			part2 = fuel
+		}
+	}
+
+	log.Printf("Part 1: %d", part1)
+	log.Printf("Part 2: %d", part2)
+}
