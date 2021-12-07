276d89ea0521d837459c03d11da2d32bfa5d6c6e
Quentin Barrand <quentin@quba.fr>
2021-12-07 23:46:47 +0100

    Move ScanCommaSeparatedInts to util.

--- day06/main.go
+++ day06/main.go
@@ -46,25 +46,6 @@ func run(start int, rounds int) (res int) {
 	return
 }
 
-func scanCommaSeparatedInts(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	for i := 0; i < len(data); i++ {
-		if data[i] == ',' {
-			return i + 1, data[:i], nil
-		}
-
-		if data[i] == '\n' {
-			return i + 1, data[:i], bufio.ErrFinalToken
-		}
-	}
-	if !atEOF {
-		return 0, nil, nil
-	}
-	// There is one final token to be delivered, which may be the empty string.
-	// Returning bufio.ErrFinalToken here tells Scan there are no more tokens after this
-	// but does not trigger an error to be returned from Scan itself.
-	return 0, data, bufio.ErrFinalToken
-}
-
 func main() {
 	cl := util.ParseCommandLine()
 
@@ -74,7 +55,7 @@ func main() {
 	fishes := make([]int, 0)
 
 	s := bufio.NewScanner(fd)
-	s.Split(scanCommaSeparatedInts)
+	s.Split(util.ScanCommaSeparatedInts)
 
 	for s.Scan() {
 		if err := s.Err(); err != nil {
---
+++ util/scan.go
@@ -0,0 +1,22 @@
+package util
+
+import "bufio"
+
+func ScanCommaSeparatedInts(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	for i := 0; i < len(data); i++ {
+		if data[i] == ',' {
+			return i + 1, data[:i], nil
+		}
+
+		if data[i] == '\n' {
+			return i + 1, data[:i], bufio.ErrFinalToken
+		}
+	}
+	if !atEOF {
+		return 0, nil, nil
+	}
+	// There is one final token to be delivered, which may be the empty string.
+	// Returning bufio.ErrFinalToken here tells Scan there are no more tokens after this
+	// but does not trigger an error to be returned from Scan itself.
+	return 0, data, bufio.ErrFinalToken
+}
